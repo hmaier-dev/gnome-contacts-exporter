233fa1a5d5d45410442a855d407b307e5975be61
Hendrik Maier <hendrik_maier@protonmail.com>
2024-11-19 12:58:21 +0100

    feat: listen for arguments in main

--- main.go
+++ main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hmaier-dev/gnome-contacts-exporter/pkg/loading"
 	"github.com/hmaier-dev/gnome-contacts-exporter/pkg/arguments"
-	"github.com/hmaier-dev/go-vcard"
 )
 
 func main(){
-
-    p := "testdata/contacts.vcf"
-    c := loading.LoadVFC(p)
-    for _, vc := range c{
-        fmt.Println(vc.AllValues(vcard.FieldFormattedName))
-        fmt.Println(vc.AllValues(vcard.FieldAddress))
-        fmt.Println(vc.AllValues(vcard.FieldEmail))
-    }
-    // buf := writer.(*bytes.Buffer)
-    // data := buf.Bytes()
-    // os.WriteFile("../testdata/contacts2.vcf", data, 0744)
+    arguments.Define()
 
 }
