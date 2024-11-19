4fec9bb1a3dee7ff78ff439001bd40a2b5826c32
Hendrik Maier <hendrik_maier@protonmail.com>
2024-11-19 10:51:38 +0100

    feat: starting to work on arguments pkg

--- pkg/arguments/arguments.go
+++ pkg/arguments/arguments.go
@@ -1,5 +1,11 @@
 package arguments
 
-func parse(){
-    return
+import(
+    "flag"
+    "github.com/hmaier-dev/gnome-contacts-exporter/pkg/loading"
+)
+
+func define(){
+    flag.Func("exporter", "Export the contacts from sqlite")
+    
 }
