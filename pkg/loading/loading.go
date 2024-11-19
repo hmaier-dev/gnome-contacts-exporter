9cc4bb1ee04af5021a26b7ab6345eba9f511ede9
Hendrik Maier <hendrik_maier@protonmail.com>
2024-11-19 15:37:49 +0100

    feat: clean loading from db stuff

--- pkg/loading/loading.go
+++ pkg/loading/loading.go
@@ -1,9 +1,6 @@
 package loading
 
 import (
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
-	"fmt"
 	"log"
     "os"
     "io"
@@ -11,11 +8,6 @@ import (
 
 )
 
-
-func Export(){
-    source := "testdata/contacts.db"
-    LoadSqlite(source)
-}
 
 // LoadVFC opens a vcf-file from the given path and stores it in a Card.
 func LoadVFC(source string) []vcard.Card {
@@ -38,61 +30,7 @@ func LoadVFC(source string) []vcard.Card {
     return out
 }
 
-type Folder_ID struct {
-	UID                  string 
-	Rev                  string 
-	FileAs               string 
-	FileAsLocalized      string 
-	Nickname             string 
-	FullName             string 
-	GivenName            string 
-	GivenNameLocalized   string 
-	FamilyName           string 
-	FamilyNameLocalized  string 
-	IsList               int    
-	ListShowAddresses    int    
-	WantsHTML            int    
-	X509Cert             int    
-	PgpCert              int    
-	VCard                string 
-	BData                string 
-}
 
-func LoadSqlite(source string){
-    db, err := sql.Open("sqlite3", source)
-    if err != nil{
-        fmt.Println(err)
-    }
-    rows, err := db.Query("Select * from folder_id WHERE vcard != NULL;")
-    if err != nil{
-        log.Fatalf("%#v\n", err)
-    } 
-    cols, err := rows.Columns()
-
-    if err != nil{
-        log.Fatalf("%#v\n", err)
-    } 
-	rawResult := make([][]byte, len(cols)) // [row][values] -> e.g. row: [[value][value][value]]
-	dest := make([]interface{}, len(cols)) // .Scan() needs []any as result type
-	allRows := make([][]string, 0)
-	for i := range cols {
-		dest[i] = &rawResult[i] // mapping dest indices to byte slice
-	}
-	for rows.Next() {
-		err := rows.Scan(dest...)
-		if err != nil {
-			log.Fatal("problems scanning the database", err)
-		}
-		singleRow := make([]string, len(cols))
-		for i, raw := range rawResult {
-			singleRow[i] = string(raw) // from byte to string
-			//fmt.Printf("%v -> %v \n", i, singleRow)
-		}
-		allRows = append(allRows, singleRow)
-	}
-    
-    fmt.Printf("%v\n", allRows)
-}
 
 func WriteVCF(cards []vcard.Card, w io.Writer){
    
