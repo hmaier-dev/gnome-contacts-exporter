8b0d9eb03a0e65476d31f9220277e1f2700b212b
Hendrik Maier <hendrik_maier@protonmail.com>
2024-11-20 14:18:04 +0100

    refactor(args): rewrote the error handling of the arguments

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 )
 
 func main(){
+    err := arguments.Define()
+    if err != nil {
+        for _, e := range err{
+            fmt.Printf("Error: %s \n", e)
+        }
+        os.Exit(1)
+    }
     // Fails if --source and --destination haven't been defined
     db.Export()
     daemon.Run()
--- pkg/arguments/arguments.go
+++ pkg/arguments/arguments.go
@@ -3,32 +3,37 @@ package arguments
 import (
 	"errors"
 	"flag"
-	"fmt"
 	"github.com/hmaier-dev/gnome-contacts-exporter/pkg/db"
+	"github.com/hmaier-dev/gnome-contacts-exporter/pkg/daemon"
 )
 
 // Sets arguments for the needed variables
-func Define() error{
+func Define() []error{
+    ret := []error{}
+
     source := flag.String("source", "", "Sets the location from where to read the database.")
 	destination := flag.String("destination", "", "Sets the location where to write the vCard-file.")
+    permit := flag.Bool("daemon", false, "Runs the exporter as daemon, which listens for changes in the database.")
     flag.Parse()
-
+    
+    // Checking mandatory flags
     if *source == "" {
-        fmt.Println("Error: Both source and destination are mandatory.")
-        fmt.Println("Usage:")
-        flag.PrintDefaults()
-        return errors.New("Source hasn't been set.")
+        ret = append(ret, errors.New("Source hasn't been set."))
     }
 
     if *destination == ""{
-        fmt.Println("Error: Both source and destination are mandatory.")
-        fmt.Println("Usage:")
-        flag.PrintDefaults()
-        return errors.New("Destination hasn't been set.")
+        ret = append(ret, errors.New("Destination hasn't been set."))
     }
 
-    db.Destination = *destination
-    db.Source = *source
-    return nil
+    if len(ret) > 0{
+        return ret
+
+    }else{
+        db.Destination = *destination
+        db.Source = *source
+        daemon.Permit = *permit
+        return nil
+    }
+
 
 }
